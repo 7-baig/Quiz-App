fbb9573b334986595b665bec3de99bf9f735f68c
Saad Baig <saadbaig7777@gmail.com>
2020-08-16 01:05:54 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styles from './App.module.css';
+import Main from './components/Main/Main';
+import welcome from './images/welcome.svg'
+import { FetchData } from './API'
 
-function App() {
+const App = () => {
+
+  const [start, setStart] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [data, setData] = useState()
+  const [number, setNumber] = useState(0)
+
+  const handleClick = async () => {
+    setStart(true)
+    setLoading(true)
+
+    const data = await FetchData()
+    setData(data)
+    setLoading(false)
+
+  }
+  const answers = (array: any[]) => [...array].sort(() => Math.random() - 0.5)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+
+        { !start 
+        ? 
+        <div className={styles.welcome}>
+        <img src={welcome} />
+        <button onClick={handleClick}>Start Quiz</button>
+        </div>
+        : null }
+
+        { 
+          start && !loading
+          ? 
+          <Main 
+            question={data[number].question}
+            setNumber={setNumber}
+            number={number}
+            answers={answers([...data[number].incorrect_answers, data[number].correct_answer])}
+            questionNm={number + 1}
+            correctAnswer={data[number].correct_answer}
+            startQuiz={handleClick}
+          /> 
+          : null 
+        }
+
     </div>
   );
 }
